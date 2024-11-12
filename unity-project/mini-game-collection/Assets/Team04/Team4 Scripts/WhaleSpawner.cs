fe0072a0121859bbd5af8d968b4f3db3ec39f021
COdinRyT <150468038+COdinRyT@users.noreply.github.com>
2024-11-12 14:49:18 -0500

    Prepping Scripts for the whale feature

--- unity-project/mini-game-collection/Assets/Team04/Team4 Scripts/WhaleSpawner.cs
+++ unity-project/mini-game-collection/Assets/Team04/Team4 Scripts/WhaleSpawner.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WhaleInteractions : MonoBehaviour
+public class WhaleSpawner : MonoBehaviour
 {
-    public float whaleSpeed = 5.0f;
-    public Vector2 targetPos;
+    public float warningTrigger; //Time it will take to trigger the warning sprite
+    public float spawnTimer; //The time it will take until the whale spawns
+    public GameObject whale;
 
     // Start is called before the first frame update
     void Start()
