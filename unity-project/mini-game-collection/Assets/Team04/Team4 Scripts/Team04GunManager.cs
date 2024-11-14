3679afb24e9d9461f0311074706e1302fa6bd989
Cyven <000912143@mohawkcollege.ca>
2024-11-14 16:03:09 -0500

    arcade controls working
    also moved to new branch, forgot i was still working on main-test, whoops

---
+++ unity-project/mini-game-collection/Assets/Team04/Team4 Scripts/Team04GunManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Team04GunManger : MonoBehaviour
+{
+    public GameObject Player1;
+    public GameObject Player2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
