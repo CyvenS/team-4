7517256d13279e9b078ec53b6b06da137f40101a
Cyven <000912143@mohawkcollege.ca>
2024-11-14 18:51:12 -0500

    Bullets added
    players can now shoot bullets
    bullets are the colour of the player that shot them, and give you knockback.
    also added "bulletproof" tag for objects like players and walls

--- unity-project/mini-game-collection/Assets/Team04/Team4 Scripts/Team04Bullet.cs
+++ unity-project/mini-game-collection/Assets/Team04/Team4 Scripts/Team04Bullet.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static MiniGameCollection.ArcadeInput;
 
 public class Team04Bullet : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (gameObject.GetComponent<Rigidbody2D>().velocity.x != 0 || gameObject.GetComponent<Rigidbody2D>().velocity.y != 0)
+        {
+            Destroy(gameObject, 2);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "Bulletproof")
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
     }
 }
