c298d43ddf550d05060d47beba98c3243e522ee9
COdinRyT <150468038+COdinRyT@users.noreply.github.com>
2024-11-19 20:59:28 -0500

    Adjusted enemy logic
    now the enemies when they spawn in will randomly target one of the players, move towards them and despawn when they enter the player's collider

--- unity-project/mini-game-collection/Assets/Team04/Team4 Scripts/Team04EnemyLogic.cs
+++ unity-project/mini-game-collection/Assets/Team04/Team4 Scripts/Team04EnemyLogic.cs
@@ -1,3 +1,4 @@
+using MiniGameCollection.Games2024.Team04;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,24 +9,35 @@ public class Team04EnemyLogic : MonoBehaviour
     public GameObject player2;
 
     [SerializeField] private float enemySpeed = 1f;
+    [SerializeField] private int PlayerID;
 
     // Start is called before the first frame update
     void Start()
     {
-    
+        int randomNum = Random.Range(1, 3);        
+        PlayerID = randomNum;
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {    
+
         TrackPlayers();        
     }
 
     void TrackPlayers()
     {
-        
-        Vector3 direction = (player1.transform.position - transform.position).normalized;
-        transform.position += direction * enemySpeed * Time.deltaTime;
+        if (PlayerID == 1)
+        {
+            Vector3 direction1 = (player1.transform.position - transform.position).normalized;
+            transform.position += direction1 * enemySpeed * Time.deltaTime;
+        }
+
+        if (PlayerID == 2)
+        {
+            Vector3 direction2 = (player2.transform.position - transform.position).normalized;
+            transform.position += direction2 * enemySpeed * Time.deltaTime;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
