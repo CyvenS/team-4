89151a5b20caf9982fab4fa68465e104428d5d8b
Cyven <000912143@mohawkcollege.ca>
2024-11-20 21:44:57 -0500

    Revert "Merge branch 'main' into Cyven-branch"
    This reverts commit d46136a95677476f4a0f78c5721e404da05f1edb, reversing
    changes made to 32bda7d37b2eda5beca9038f7fa24929a2cbf070.

--- unity-project/mini-game-collection/Assets/Team04/Team4 Scripts/Team04EnemyLogic.cs
+++ unity-project/mini-game-collection/Assets/Team04/Team4 Scripts/Team04EnemyLogic.cs
@@ -1,51 +1,18 @@
-using MiniGameCollection.Games2024.Team04;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace MiniGameCollection.Games2024.Team04
+public class Team04EnemyLogic : MonoBehaviour
 {
-    public class Team04EnemyLogic : MonoBehaviour
+    // Start is called before the first frame update
+    void Start()
     {
-        public GameObject player1;
-        public GameObject player2;
+        
+    }
 
-        [SerializeField] private float enemySpeed = 1f;
-        [SerializeField] private int PlayerID;
-
-        // Start is called before the first frame update
-        void Start()
-        {
-            int randomNum = Random.Range(1, 3);
-            PlayerID = randomNum;
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-            TrackPlayers();
-        }
-
-        void TrackPlayers()
-        {
-            if (PlayerID == 1)
-            {
-                Vector3 direction1 = (player1.transform.position - transform.position).normalized;
-                transform.position += direction1 * enemySpeed * Time.deltaTime;
-            }
-
-            if (PlayerID == 2)
-            {
-                Vector3 direction2 = (player2.transform.position - transform.position).normalized;
-                transform.position += direction2 * enemySpeed * Time.deltaTime;
-            }
-        }
-
-        private void OnTriggerEnter2D(Collider2D collision)
-        {
-            Destroy(gameObject);
-        }
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
-
