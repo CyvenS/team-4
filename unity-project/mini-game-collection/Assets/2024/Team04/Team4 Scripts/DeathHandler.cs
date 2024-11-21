2180b9fbbab04b3f6d833e9236202909ff8248b3
Cyven <000912143@mohawkcollege.ca>
2024-11-21 18:22:53 -0500

    small bug fixes

--- unity-project/mini-game-collection/Assets/2024/Team04/Team4 Scripts/DeathHandler.cs
+++ unity-project/mini-game-collection/Assets/2024/Team04/Team4 Scripts/DeathHandler.cs
@@ -3,48 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class DeathHandler : MonoBehaviour
+namespace MiniGameCollection.Games2024.Team04
 {
-    public GameObject playerWin;
-    // Start is called before the first frame update
-    void Start()
+    public class DeathHandler : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (playerWin.activeSelf)
+        public GameObject playerWin;
+        // Start is called before the first frame update
+        void Start()
         {
-            gameObject.GetComponent<CircleCollider2D>().enabled = false;
-            gameObject.transform.position = new Vector3(0, 0, -6);
-            gameObject.transform.localScale = new Vector3(2, 2, gameObject.transform.localScale.z);
-            
 
-            if (Input.GetKey(KeyCode.Q) && Input.GetKey(KeyCode.E) && Input.GetKey(KeyCode.Period) && Input.GetKey(KeyCode.Comma))
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (playerWin.activeSelf)
             {
-                string currentSceneName = SceneManager.GetActiveScene().name;
-                SceneManager.LoadScene(currentSceneName);
+                gameObject.GetComponent<CircleCollider2D>().enabled = false;
+                gameObject.transform.position = new Vector3(0, 0, -6);
+                gameObject.transform.localScale = new Vector3(2, 2, gameObject.transform.localScale.z);
+
+
+                if (Input.GetKey(KeyCode.Q) && Input.GetKey(KeyCode.E) && Input.GetKey(KeyCode.Period) && Input.GetKey(KeyCode.Comma))
+                {
+                    string currentSceneName = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene(currentSceneName);
+                }
             }
         }
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (playerWin.activeSelf == false && collision.gameObject.name != "2024-team04-wall1" && collision.gameObject.name != "2024-team04-wall2" && collision.gameObject.name != "2024-team04-wall3" && collision.gameObject.name != "2024-team04-wall4" && collision.gameObject.name != "2024-team04-bullet(Clone)")
+        private void OnCollisionEnter2D(Collision2D collision)
         {
-            playerWin.SetActive(true);
-            Debug.Log(gameObject.name);
-            Destroy(gameObject);
+            if (playerWin.activeSelf == false && collision.gameObject.name != "2024-team04-wall1" && collision.gameObject.name != "2024-team04-wall2" && collision.gameObject.name != "2024-team04-wall3" && collision.gameObject.name != "2024-team04-wall4" 
+                && collision.gameObject.name != "2024-team04-bullet(Clone)" && collision.gameObject.name != "2024-team04-player1" && collision.gameObject.name != "2024-team04-player2")
+            {
+                playerWin.SetActive(true);
+                Debug.Log(gameObject.name);
+                Destroy(gameObject);
+            }
         }
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (playerWin.activeSelf == false && collision.gameObject.name == "2024-team04-whale-1" || collision.gameObject.name == "2024-team04-whale-2" || collision.gameObject.name == "2024-team04-whale-3" || collision.gameObject.name == "2024-team04-whale-4")
+        private void OnTriggerEnter2D(Collider2D collision)
         {
-            playerWin.SetActive(true);
-            Debug.Log(gameObject.name);
-            Destroy(gameObject);
+            if (playerWin.activeSelf == false && collision.gameObject.name == "2024-team04-whale-1" || collision.gameObject.name == "2024-team04-whale-2" || collision.gameObject.name == "2024-team04-whale-3" || collision.gameObject.name == "2024-team04-whale-4")
+            {
+                playerWin.SetActive(true);
+                Debug.Log(gameObject.name);
+                Destroy(gameObject);
+            }
         }
     }
 }
